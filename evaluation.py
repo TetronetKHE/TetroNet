cea6e56ffc14498a2c66fd2be1bfded227fb54ae
opfromthestart <opfromthestart@gmail.com>
2019-09-28 15:33:20 -0400

    Update evaluation.py
    modified for Game

--- evaluation.py
+++ evaluation.py
@@ -5,19 +5,20 @@ np.random.seed(1984)
 
 def train(model, steps, gamma):
     for asdfghjkl in range(steps):
-        games = [[getStart(),[0,0,0,0,0]]
-        scores = [0]
+        games = []
+        lastGame=Game()
+        scores = []
         lost=False
         after = [0,0,0]
         while not lost:
-            lstGame=games[-1][0]
             inpScores=[0,0,0,0,0]
             for i in range(6):
                 inTest = [j==i for j in range(5)]
-                inpScores[i]=model.predict([[[lstGame.toList()+inTest]]])
+                inpScores[i]=model.predict([[[getGameState(lastGame)+inTest]]])
             moves = inpScores.index(max(inpScores))
-            after = lstGame*moves
-            games += [[after[0],moves]]
+            after = lastGame*moves
+            games += [[getGameState(lastGame),moves]]
+            lastGame=after[0]
             if after[1] or after[2]:
                 pt = after[1] - 20*after[2]
                 for i in range(len(scores)):
