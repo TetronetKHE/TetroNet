49bb0a50b0b843859a1017b8dfb8615c6c0b16ea
opfromthestart <opfromthestart@gmail.com>
2019-09-28 14:11:42 -0400

    Inputs
    Reduced number of inputs.

--- evaluation.py
+++ evaluation.py
@@ -5,11 +5,12 @@ def copyMat(a):
     return n
 
 class gameState:
-    def __init__(self, staticMatrix, pieceID, rotation, position):
+    def __init__(self, staticMatrix, pieceID, rotation, position, queued):
         self.static = staticMatrix  #10x20 matrix
         self.piece = pieceID        #0-6
         self.rotation = rotation    #r 0-3
         self.pos = position         #[x,y] x 0-9 y 0-19
+        self.nexts = queued         #[ID, ID, ID]
     def __mul__(self, inpt):
         mat = copyMat(self.static)
         new = gameState(mat,self.piece,self.rotation,[self.pos[0],self.pos[1]])
@@ -22,6 +23,7 @@ class gameState:
         a+=[self.piece]
         a+=[self.rotation]
         a+=self.pos
+        a+=self.nexts
         return a
 
 np.random.seed(1984)
@@ -31,15 +33,15 @@ def getStart():
 
 def train(model, steps, gamma):
     for asdfghjkl in range(steps):
-        games = [[getStart(),[0,0,0,0,0,0,0]]
+        games = [[getStart(),[0,0,0,0,0]]
         scores = [0]
         lost=False
         after = [0,0,0]
         while not lost:
             lstGame=games[-1][0]
-            inpScores=[0,0,0,0,0,0,0]
-            for i in range(7):
-                inTest = [j==i for j in range(7)]
+            inpScores=[0,0,0,0,0]
+            for i in range(6):
+                inTest = [j==i for j in range(5)]
                 inpScores[i]=model.predict([[[lstGame.toList()+inTest]]])
             moves = inpScores.index(max(inpScores))
             after = lstGame*moves
