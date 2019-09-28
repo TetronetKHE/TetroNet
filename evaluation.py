0c7dae57cb0cf5d66ec3039727ef29048199ec05
opfromthestart <opfromthestart@gmail.com>
2019-09-27 23:35:41 -0400

    Update evaluation.py
    fixed syntax

--- evaluation.py
+++ evaluation.py
@@ -1,6 +1,7 @@
-class gamestate:
+class gameState:
     def __init__(self, staticMatrix, pieceID, rotation, position):
         self.static = staticMatrix  #10x20 matrix
         self.piece = pieceID        #0-6
         self.rotation = rotation    #r 0-3
         self.pos = position         #[x,y] x 0-9 y 0-19
+
