f74952e8e48ddb786bb4817a75c306d14f857e36
opfromthestart <opfromthestart@gmail.com>
2019-09-28 09:53:16 -0400

    Evaluation function done?
    Im pretty sure it should work now if the game works how it should.

--- evaluation.py
+++ evaluation.py
@@ -30,10 +30,24 @@ def getStart():
     true=False #make a random start
 
 def train(model, steps, gamma):
-    for i in range(steps):
-        games = [getStart()]
+    for asdfghjkl in range(steps):
+        games = [[getStart(),[0,0,0,0,0,0,0]]
         scores = [0]
         lost=False
-        after = [0,0]
+        after = [0,0,0]
         while not lost:
-            after = games[-1]*
+            lstGame=games[-1][0]
+            inpScores=[0,0,0,0,0,0,0]
+            for i in range(7):
+                inTest = [j==i for j in range(7)]
+                inpScores[i]=model.predict([[[lstGame.toList()+inTest]]])
+            moves = inpScores.index(max(inpScores))
+            after = lstGame*moves
+            games += [[after[0],moves]]
+            if after[1] or after[2]:
+                pt = after[1] - 20*after[2]
+                for i in range(len(scores)):
+                    scores[-1-i] += pt*gamma**i
+            if after[2]:
+                lost=True
+        model.fit([[[[games[i][0].toList()+games[i][1]]] for i in range(len(games))]],[[[i] for i in scores]], epochs=50)
--- tetroNet.py
+++ tetroNet.py
@@ -10,7 +10,7 @@ from rl.policy import EpsGreedyQPolicy
 from rl.memory import SequentialMemory
 
 model = Sequential()
-model.add(Flatten(input_shape=(1,204)))
+model.add(Flatten(input_shape=(1,211)))
 model.add(Dense(30))
 model.add(Dense(7))
 model.add(Activation('linear'))
