a1f7daef2299fd836193fd239f32aeee64d40f6c
opfromthestart <opfromthestart@gmail.com>
2019-09-27 23:28:56 -0400

    Update tetroNet.py

--- tetroNet.py
+++ tetroNet.py
@@ -15,14 +15,3 @@ model.add(Dense(16))
 model.add(Dense(7))
 model.add(Activation('linear'))
 print(model.summary())
-
-policy = EpsGreedyQPolicy()
-memory = SequentialMemory(limit=50000, window_length=1)
-dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=10,
-target_model_update=1e-2, policy=policy)
-dqn.compile(Adam(lr=1e-3), metrics=['mae'])
-
-# Okay, now it's time to learn something! We visualize the training here for show, but this slows down training quite a lot. 
-dqn.fit(env, nb_steps=5000, visualize=True, verbose=2)
-
-dqn.test(env, nb_episodes=5, visualize=True)
