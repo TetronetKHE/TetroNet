2611eac138767fce9b2fd57ae3f0eac88afd1e00
VonnW <vonn.wheatley@gmail.com>
2019-09-28 15:11:00 -0400

    more gaming

--- game/main.py
+++ game/main.py
@@ -1,5 +1,9 @@
 import sys, pygame, os
-# import numpy as np
+import numpy as np
+import random
+import copy
+
+import tilemap, board, piecedata
 
 Tetrominos = {
         "I" : 0,
@@ -8,7 +12,8 @@ Tetrominos = {
         "O" : 3,
         "S" : 4,
         "T" : 5,
-        "Z" : 6
+        "Z" : 6,
+        "Length": 7
 }
 
 class TileMap:
@@ -54,7 +59,7 @@ class Board:
                 self.tileMap.draw(surface, 0, 0)
 
         def getBoringBoard(self):
-                return [[(self.tileMap.data[y][x] > 0) for x in range(self.width)] for y in range(self.height)]
+                return [((self.tileMap.data[y][x] > 0) for x in range(self.width)) for y in range(self.height)]
 
 class PieceData:
         TetrominoTable = [
@@ -105,23 +110,33 @@ class PieceData:
                 
                 return result
         
+        def getOffsetTableLength(tetromino, rotation):
+                if tetromino == Tetrominos["I"]:
+                        return len(PieceData.RotationTable["I"])
+                elif tetromino == Tetrominos["O"]:
+                        return len(PieceData.RotationTable["O"])
+                else:
+                        return len(PieceData.RotationTable["Other"])
+        
         def getOffsetPoint(tetromino, rotation, check):
                 if tetromino == Tetrominos["I"]:
-                        result = PieceData.RotationTable["I"][check]
+                        result = PieceData.RotationTable["I"][check].copy()
                 elif tetromino == Tetrominos["O"]:
-                        result = PieceData.RotationTable["O"][check]
+                        result = PieceData.RotationTable["O"][check].copy()
                 else:
-                        result = PieceData.RotationTable["Other"][check]
+                        result = PieceData.RotationTable["Other"][check].copy()
                 
                 return result
         
-        def getOffsetPoints(tetromino, prevRot, rotation, check):
+        def getOffset(tetromino, prevRot, rotation, check):
                 prevOffset = PieceData.getOffsetPoint(tetromino, rotation, check)
                 nextOffset = PieceData.getOffsetPoint(tetromino, rotation, check)
                 
                 return (prevOffset[0] - nextOffset[0], prevOffset[1] - nextOffset[1])
 
 class Piece:
+        MaxLockDelay = 4
+        
         def __init__(self, board, tetromino):
                 self.x = 4
                 self.y = 20
@@ -131,10 +146,26 @@ class Piece:
                 self.tetromino = tetromino
                 self.rotation = 0
                 
+                self.lockDelay = Piece.MaxLockDelay
+                
                 self.points = PieceData.getPiecePoints(self.tetromino, self.rotation)
         
-        def update(self, moveLeft, moveRight, rotateLeft, rotateRight, softDrop, hardDrop):
-                pass
+        def update(self, inputs):
+                if inputs[0]: self.tryMove(-1, 0)
+                if inputs[1]: self.tryMove(1, 0)
+                
+                if inputs[2]: self.tryRotation(False)
+                if inputs[3]: self.tryRotation(True)
+                
+                if inputs[4]:
+                        self.y = self.getYDropCoord()
+                elif tryMove(0, -1):
+                        self.lockDelay = Piece.MaxLockDelay
+                else:
+                        if self.lockDelay:
+                                self.lockDelay -= 1
+                        else:
+                                self.placeDown()
         
         def draw(self, surface):
                 for i in range(len(points)):
@@ -149,10 +180,93 @@ class Piece:
                 return self.fitAbsolute(self.x + x, self.y + y)
         
         def tryMove(self, x, y):
-                pass
+                if fit(self, x, y):
+                        self.x += x
+                        self.y += y
+                        return True
+                return False
                 
-        def tryRotation(self, direction):
-                pass
+        def tryRotation(self, clockwise):
+                prevRot = self.rotation
+                if clockwise:
+                        self.rotation = (self.rotation + 1) % 4
+                else:
+                        self.rotation = (self.rotation + 3) % 4
+                self.points = PieceData.getPiecePoints(self.tetromino, self.rotation)
+                
+                for i in range(PieceData.getOffsetTableLength(self.tetromino, self.rotation)):
+                        offset = PieceData.getOffset(self.tetromino, prevRot, self.rotation, i)
+                        
+                        if self.fit(offset[0], offset[1]):
+                                self.x += offset[0]
+                                self.y += offset[1]
+                                return True
+                        
+                self.rotation = prevRot
+                self.points = PieceData.getPiecePoints(self.tetromino, self.rotation)
+                
+                return False
+        
+        def getYDropCoord(self):
+                tmpY = self.y
+                
+                while True:
+                        if self.fitAbsolute(self.x, tmpY - 1):
+                                tmpY -= 1
+                        else:
+                                return tmpY
+        
+        def getBoringPiece(self):
+                return [self.tetromino, self.rotation, self.x, self.y]
+
+class Bag:
+        def __init__(self):
+                self.getNewBag()
+        
+        def getNewBag(self):
+                self.bag = [i for i in range(Tetrominos["Length"])]
+                random.shuffle(self.bag)
+        
+        def getPiece(self):
+                if not len(self.bag): self.getNewBag()
+                return self.bag.pop(0)
+
+class NextQueue:
+        MaxNext = 3
+        
+        def __init__(self, bag):
+                self.bag = bag
+                self.getNewQueue()
+        
+        def getNewQueue(self):
+                self.queue = [self.bag.getPiece() for i in range(NextQueue.MaxNext)]
+        
+        def getPiece(self):
+                if not len(self.queue): self.getNewQueue()
+                return self.queue.pop(0)
+        
+        def getBoringNext(self):
+                return self.queue.copy()
+
+class Game:
+        def __init__(self):
+                self.board = Board()
+                self.bag = Bag()
+                self.nextQueue = NextQueue(self.bag)
+                self.piece = Piece(self.board, self.nextQueue.getPiece())
+        
+        def update(self, inputs):
+                self.piece.update()
+                if self.piece.down: self.piece = Piece(board, nextQueue.getPiece())
+        
+        def tryUpdate(self, inputs):
+                tmpBoard = Board()
+                tmpBoard.tileMap.data = [row.copy() for row in self.board.tileMap.data]
+                tmpBag = Bag()
+                tmpBag.bag = self.bag.bag.copy()
+                tmpNextQueue = NextQueue(tmpBag)
+                #TODO
+                
 
 # Start pygame.
 pygame.init()
@@ -165,14 +279,34 @@ size = (width, height)
 screen = pygame.display.set_mode(size)
 
 # NOTE: This is using screen coordinates.  Y is down internally, but my wrapper functions for the board will automatically convert the coordinates into board coordinates.
-board = Board()
-piece = Piece(board, Tetrominos["T"])
+def getANewGame():
+        board = Board()
+        bag = Bag()
+        nextQueue = NextQueue(bag)
+        piece = Piece(board, nextQueue.getPiece())
+        
+        return board, bag, nextQueue, piece
+
+def getGameState(game):
+        result = []
+        result += board.getBoringBoard()
+        result += piece.getBoringPiece()
+        result += nextQueue.getBoringNext()
+        return result
+
+def tryUpdate():
+        
+        
+
+board, bag, nextQueue, piece = getANewGame()
 
 while True:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                         sys.exit()
         
+        
         screen.fill((0, 0, 0))
         board.draw(screen)
+        piece.draw(screen)
         pygame.display.flip()
