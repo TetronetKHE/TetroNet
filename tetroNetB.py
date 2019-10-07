c890688d089b5c3d2bcd3284f91f881bf8a2c95f
opfromthestart <opfromthestart@gmail.com>
2019-10-07 10:17:43 -0400

    better training?

--- tetroNetB.py
+++ tetroNetB.py
@@ -2,4 +2,4 @@ import evaluationB as evaluation
 import ai
 
 aiInst = ai.AI(strategy=1, saveFileName="tetroNetBackupB")
-aiInst.train(saves=300, steps=50, gamma=.99, tryhard=0.01,epochs=5, evaluation=evaluation)
+aiInst.train(saves=2500, steps=2, gamma=.99, tryhard=0.01,epochs=1, freq=10, evaluation=evaluation)
